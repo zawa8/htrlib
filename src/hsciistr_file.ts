5fac5f46c2479a5a3e7b482831a0570196da8631
viml <vimalkg@gmail.com>
2025-07-27 16:33:11 +0530

    fb.com/ztrabc

--- src/hsciistr_file.ts
+++ src/hsciistr_file.ts
@@ -444,8 +444,8 @@ export class hsciistr {
   u2idict = {
     all_phoniks_list: [
       '', // 	ऀ	900	2304		inverted candrabindu
-      'MM', // 	ँ	901	2305		anunasika(candrabindu)
-      'MM', // 	ं ń	902	2306	anuswara	anusvara bindu
+      '', // 	ँ	901 N	2305		anunasika(candrabindu) 
+      '', // 	ं ń	902	2306	anuswara	anusvara bindu // लड़कियां भाग कर शादी कर रहें
       ':', // 	ः	903	2307		visarga
       'xe', // 	ऄ à	904	2308		short a ,  e in awadh
       'x', // 	अ	905	2309	vovls
